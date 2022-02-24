9eda5996f406d71245b5e68c873be2e296dd77fc
IT20012342 <it20012342@my.sliit.lk>
2022-02-24 10:31:52 +0530

    Person class added

---
+++ HelloWorld/src/com/sliit/main/Person.java
@@ -0,0 +1,11 @@
+package com.sliit.main;
+
+public class Person {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("My name is Isuru");
+	}
+
+}
